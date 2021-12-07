320ea95a2284239ada8817748737670db21e33a7
Chaitra-Bhat383 <79207846+Chaitra-Bhat383@users.noreply.github.com>
2021-12-07 10:12:13 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
     return ( 
         <div >
         <div className = "Navigation" >
-        <span className = "navtext" > RESUME PARSER </span> <
-        div className = "fourth" >
+        <span className = "navtext" ><a style={{textDecoration:"none",color:"black"}} href="https://github.com/Chaitra-Bhat383/Resume-Parser"> RESUME PARSER </a></span> 
+        <div className = "fourth" >
         <NavLink to = "/home" style={{textDecoration:"none",color:"black"}}> <span className = "navtext"> HOME </span></NavLink >
         <NavLink to = "/about" style={{textDecoration:"none",color:"black"}} > <span className = "navtext" > ABOUT </span></NavLink >
         <NavLink to = "/stats" style={{textDecoration:"none",color:"black"}} > <span className = "navtext" > STATS </span></NavLink >
